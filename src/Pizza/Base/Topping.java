9a03d0add5c52b35ce0a6fa888d768fee62be095
CASTILLOAXEL <140232308+CASTILLOAXEL@users.noreply.github.com>
2023-09-17 08:25:14 -0600

    Add files via upload

--- src/Pizza/Base/Topping.java
+++ src/Pizza/Base/Topping.java
@@ -3,11 +3,11 @@ package Pizza.Base;
 public class Topping {
     private String nombre;
     private double precio;
- public static final Topping Piña = new Topping("Piña", 20);
+    public static final Topping Piña = new Topping("Piña", 20);
     public static final Topping Mozarella = new Topping("Mozzarella", 15);
     public static final Topping Jamon = new Topping("Jamon", 25);
-public static final Topping Polllo = new Topping("Pollo", 10);
-public static final Topping SalsaBarbacoa = new Topping("Salsa Barbacoa", 20);
+    public static final Topping Polllo = new Topping("Pollo", 10);
+    public static final Topping SalsaBarbacoa = new Topping("Salsa Barbacoa", 20);
     public static final Topping Cebolla = new Topping("Cebolla", 12);
     public static final Topping Tomate = new Topping("Tomate", 10);
     public static final Topping Albahaca = new Topping("Albahaca", 5);
@@ -19,6 +19,7 @@ public static final Topping SalsaBarbacoa = new Topping("Salsa Barbacoa", 20);
     public static final Topping Pepperoni = new Topping("Pepperoni", 25);
     public static final Topping Frijoles = new Topping("Frijoles", 25);
     public static  final Topping Queso = new Topping("Queso", 20);
+
     public Topping(String nombre, double precio) {
         this.nombre = nombre;
         this.precio = precio;
@@ -31,6 +32,10 @@ public static final Topping SalsaBarbacoa = new Topping("Salsa Barbacoa", 20);
 
     }
 
+    @Override
+    public String toString() {
+        return nombre + " Q" + precio;
+    }
 
     public String getNombre() {
         return nombre;
